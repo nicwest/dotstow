b6ded4c7784649d93ea624c4a0140ce9d97a087c
Nic West <nicwest.cb@gmail.com>
2016-08-14 21:09:53 +0100

    export fish env's propperly

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,5 @@
-set PATH /home/nic/bin $PATH
-set EDITOR vim
+set -x PATH /home/nic/bin $PATH
+set -x EDITOR vim
 
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_get_prompt_showcolorhints 'yes'
