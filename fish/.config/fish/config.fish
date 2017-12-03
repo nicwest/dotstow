3a697b8a808ddd492dabf8a971548f5aa80672be
Nic West <nicwest@mailbox.org>
2017-12-03 02:17:27 +0000

    init valsheda branch

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,10 +1,7 @@
-
 set -x PATH $HOME/bin $HOME/.gem/ruby/2.4.0/bin $HOME/go/bin $PATH 
 set -x EDITOR vim
-set -x GOPATH ~/go
+set -x GOPATH $HOME/go
 set -x TERM 'xterm-256color'
 set -x LANG en_GB.UTF-8
 
 source /usr/share/autojump/autojump.fish
-
-
