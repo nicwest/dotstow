37612f0a1324c2dcee87b090ee48badfcd699aee
Nic West <nicwest@mailbox.org>
2017-12-24 12:22:19 +0000

    add lein auto

--- leiningen/.lein/profiles.clj
+++ leiningen/.lein/profiles.clj
@@ -1,3 +1,4 @@
 {:user {:dependencies [[slamhound "RELEASE"]]
         :plugins [[lein-kibit "0.1.2"]
-                  [jonase/eastwood "0.2.5"]]}}
+                  [jonase/eastwood "0.2.5"]
+                  [lein-auto "0.1.3"]]}}
